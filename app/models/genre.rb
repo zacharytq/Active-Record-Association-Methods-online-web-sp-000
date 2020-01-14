6dfe101d161ca43573433ec5a68d809d224e2ac5
chic-page-6502 <zacharytq@gmail.com>
2020-01-14 01:22:26 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.uniq
+    self.artists.map{|i| i.name}
   end
 end
