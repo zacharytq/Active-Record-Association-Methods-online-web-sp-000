0a5fd7d441344dca7e76b6ddff19ed7e125754de
chic-page-6502 <zacharytq@gmail.com>
2020-01-14 01:21:57 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,10 +9,11 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.artists.uniq.size
+    self.artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.uniq
   end
 end
